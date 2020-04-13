8c94ed537334ce7d2333752454b3e46d456a4047
tpet93 <tpet93@gmail.com>
2020-04-13 12:18:22 +1000

    Update findstems.cpp
    added verbosity during  cylinder finding to help fine tune parameters
    added inital filter for non vertical cylinders.
    autoformatting caused extra change issues

--- src/findstems.cpp
+++ src/findstems.cpp
@@ -4,60 +4,31 @@
 
 #include <pcl/io/pcd_io.h>
 #include <pcl/common/common.h>
+#include <algorithm>    // std::max
 
-int main (int argc, char *argv[])
+
+const std::string reset("\033[0m");
+const std::string red("\033[0;31m");
+const std::string green("\033[0;32m");
+const std::string cyan("\033[0;96m");
+
+
+int main(int argc, char *argv[])
 {
-	pcl::PCDReader reader;
-	pcl::PCDWriter writer;
-	std::stringstream ss;
-	//
-	std::cout << "Reading slice: " << std::flush;
-	std::vector<std::string> id = getFileID(argv[5]);
-	pcl::PointCloud<PointTreeseg>::Ptr slice(new pcl::PointCloud<PointTreeseg>);
-	reader.read(argv[5],*slice);
-	std::cout << "complete" << std::endl;
-	//
-	std::cout << "Cluster extraction: " << std::flush;
-	std::vector<pcl::PointCloud<PointTreeseg>::Ptr> clusters;
-	int nnearest = 18;
-	int nmin = 100;
-	std::vector<float> nndata = dNN(slice,nnearest);
-	euclideanClustering(slice,nndata[0],nmin,clusters);
-	ss.str("");
-	ss << id[0] << ".intermediate.slice.clusters.pcd";
-	writeClouds(clusters,ss.str(),false);
-	std::cout << ss.str() << " | " << clusters.size() << std::endl;
-	//
-	std::cout << "Region-based segmentation: " << std::flush;
-	std::vector<pcl::PointCloud<PointTreeseg>::Ptr> regions;
-	nnearest = 9;
-	nmin = 100;
-	float smoothness = atof(argv[1]);
-	for(int i=0;i<clusters.size();i++)
-	{
-		std::vector<pcl::PointCloud<PointTreeseg>::Ptr> tmpregions;
-		regionSegmentation(clusters[i],nnearest,nmin,smoothness,tmpregions);
-		for(int j=0;j<tmpregions.size();j++) regions.push_back(tmpregions[j]);
-	}
-	ss.str("");
-	ss << id[0] << ".intermediate.slice.clusters.regions.pcd";
-	writeClouds(regions,ss.str(),false);
-	std::cout << ss.str() << " | " << regions.size() << std::endl;
-	//
-	std::cout << "RANSAC cylinder fits: " << std::flush;
-	std::vector<pcl::PointCloud<PointTreeseg>::Ptr> cyls;
-	nnearest = 60;
-	float dmin = atof(argv[2]);
-	float dmax = atof(argv[3]);
+
 	std::ifstream coordfile;
-	coordfile.open(argv[4]);
-	float coords[4];
+	std::cout << "coordfile: " << argv[5] << std::endl;
+
+	coordfile.open(argv[5]);
+
+	float coords[4] = {0};
 	int n = 0;
-	if(coordfile.is_open())
+	if (coordfile.is_open())
 	{
-		while(!coordfile.eof())
+		while (!coordfile.eof())
 		{
 			coordfile >> coords[n];
+
 			n++;
 		}
 	}
@@ -66,55 +37,142 @@ int main (int argc, char *argv[])
 	float xmax = coords[1];
 	float ymin = coords[2];
 	float ymax = coords[3];
-	float lmin = 2.25; //assuming 3m slice
-	float stepcovmax = 0.2;
-	float radratiomin = 0.8;
-	for(int i=0;i<regions.size();i++)
+
+	std::cout << xmin << " " << xmax << " " << ymin << " " << ymax << std::endl;
+
+	pcl::PCDReader reader;
+	pcl::PCDWriter writer;
+	std::stringstream ss;
+	//
+	std::cout << "Reading slice: " << std::flush;
+	std::vector<std::string> id = getFileID(argv[6]);
+	pcl::PointCloud<PointTreeseg>::Ptr slice(new pcl::PointCloud<PointTreeseg>);
+	reader.read(argv[6], *slice);
+	std::cout << "complete" << std::endl;
+	//
+	std::cout << "Cluster extraction: " << std::flush;
+	std::vector<pcl::PointCloud<PointTreeseg>::Ptr> clusters;
+	int nnearest = 18;
+	int nmin = 100;
+	std::vector<float> nndata = dNN(slice, nnearest);
+	euclideanClustering(slice, nndata[0], nmin, clusters);
+	ss.str("");
+	ss << id[0] << ".intermediate.slice.clusters.pcd";
+	writeClouds(clusters, ss.str(), false);
+	std::cout << ss.str() << " | " << clusters.size() << std::endl;
+	//
+	std::cout << "Region-based segmentation: " << std::flush;
+	std::vector<pcl::PointCloud<PointTreeseg>::Ptr> regions;
+	nnearest = 9;
+	nmin = 100;
+	float smoothness = atof(argv[1]);
+	for (int i = 0; i < clusters.size(); i++)
 	{
+		std::vector<pcl::PointCloud<PointTreeseg>::Ptr> tmpregions;
+		regionSegmentation(clusters[i], nnearest, nmin, smoothness, tmpregions);
+		for (int j = 0; j < tmpregions.size(); j++)
+			regions.push_back(tmpregions[j]);
+	}
+	ss.str("");
+	ss << id[0] << ".intermediate.slice.clusters.regions.pcd";
+	writeClouds(regions, ss.str(), false);
+	std::cout << ss.str() << " | " << regions.size() << std::endl;
+	//
+	std::cout << "RANSAC cylinder fits: " << std::flush;
+	std::vector<pcl::PointCloud<PointTreeseg>::Ptr> cyls;
+	nnearest = 60;
+	float dmin = atof(argv[2]);
+	float dmax = atof(argv[3]);
+
+	float lmin = atof(argv[5]) * 0.75; //assuming 3m slice == 3/4 slice h
+	float stepcovmax = 0.1;
+	float radratiomin = 0.8;
+	float maxangle = 0.3;
+
+	int cylnum = 0;
+	for (int i = 0; i < regions.size(); i++)
+	{
+		std::cout << "Region : " << i << "\t of : " << regions.size() << std::flush;
+
 		cylinder cyl;
-		fitCylinder(regions[i],nnearest,true,true,cyl);
-		if(cyl.ismodel == true)
-		{		
-			if(cyl.rad*2 >= dmin && cyl.rad*2 <= dmax && cyl.len >= lmin)
+
+		// if(i%10 ==0)
+		// {
+		fitCylinder(regions[i], nnearest, true, true, cyl);
+
+		if (cyl.ismodel == true)
+		{
+			if (cyl.rad * 2 >= dmin && cyl.rad * 2 <= dmax && cyl.len >= lmin)
 			{
-				if(cyl.stepcov <= stepcovmax && cyl.radratio > radratiomin)
+
+				std::cout << "  cov: " << cyl.stepcov << std::flush;
+				std::cout << "  max_dir: " << std::max(abs(cyl.dx),abs(cyl.dy)) << std::flush;
+
+				if (cyl.stepcov <= stepcovmax)
 				{
-					if(cyl.x >= xmin && cyl.x <= xmax)
+					if (cyl.radratio > radratiomin)
 					{
-						if(cyl.y >= ymin && cyl.y <= ymax)
+						if ((cyl.x >= xmin && cyl.x <= xmax) && (cyl.y >= ymin && cyl.y <= ymax))
 						{
-							cyls.push_back(cyl.inliers);
+							if ((abs(cyl.dx) < maxangle) && (abs(cyl.dy) < maxangle) )
+							{
+								cyls.push_back(cyl.inliers);
+								std::cout << green << "  Added Cylinder : " << cylnum << reset <<std::flush;
+								cylnum++;
+
+							}
+							else
+							{
+								std::cout << "  Bad angle" << std::flush;
+							}
+						}
+						else
+						{
+							std::cout << "  Outside Coords :" << std::flush;
 						}
 					}
+					else
+					{
+						std::cout << "  radratio too large," << std::flush;
+					}
+				}
+				else
+				{
+					std::cout << "  cov too large," << std::flush;
 				}
 			}
 		}
+		// }
+		std::cout << std::endl;
 	}
+
 	ss.str("");
 	ss << id[0] << ".intermediate.slice.clusters.regions.cylinders.pcd";
-	writeClouds(cyls,ss.str(),false);
+	writeClouds(cyls, ss.str(), false);
 	std::cout << ss.str() << " | " << cyls.size() << std::endl;
 	//
 	std::cout << "Principal component trimming: " << std::flush;
 	float anglemax = 35;
 	std::vector<int> idx;
-	for(int j=0;j<cyls.size();j++)
+	for (int j = 0; j < cyls.size(); j++)
 	{
 		Eigen::Vector4f centroid;
 		Eigen::Matrix3f covariancematrix;
 		Eigen::Matrix3f eigenvectors;
 		Eigen::Vector3f eigenvalues;
-		computePCA(cyls[j],centroid,covariancematrix,eigenvectors,eigenvalues);
-		Eigen::Vector4f gvector(eigenvectors(0,2),eigenvectors(1,2),0,0);
-		Eigen::Vector4f cvector(eigenvectors(0,2),eigenvectors(1,2),eigenvectors(2,2),0);
-		float angle = pcl::getAngle3D(gvector,cvector) * (180/M_PI);
-		if(angle >= (90 - anglemax) || angle <= (90 + anglemax)) idx.push_back(j);
+		computePCA(cyls[j], centroid, covariancematrix, eigenvectors, eigenvalues);
+		Eigen::Vector4f gvector(eigenvectors(0, 2), eigenvectors(1, 2), 0, 0);
+		Eigen::Vector4f cvector(eigenvectors(0, 2), eigenvectors(1, 2), eigenvectors(2, 2), 0);
+		float angle = pcl::getAngle3D(gvector, cvector) * (180 / M_PI);
+		if (angle >= (90 - anglemax) || angle <= (90 + anglemax))
+			idx.push_back(j);
 	}
 	std::vector<pcl::PointCloud<PointTreeseg>::Ptr> pca;
-        for(int k=0;k<idx.size();k++) pca.push_back(cyls[idx[k]]);	
+	for (int k = 0; k < idx.size(); k++)
+		pca.push_back(cyls[idx[k]]);
 	ss.str("");
 	ss << id[0] << ".intermediate.slice.clusters.regions.cylinders.principal.pcd";
-	writeClouds(pca,ss.str(),false);
+	writeClouds(pca, ss.str(), false);
 	std::cout << ss.str() << " | " << pca.size() << std::endl;
 	//
 	std::cout << "Concatenating stems: " << std::flush;
@@ -124,12 +182,12 @@ int main (int argc, char *argv[])
 	catIntersectingClouds(stems);
 	ss.str("");
 	ss << id[0] << ".intermediate.slice.clusters.regions.cylinders.principal.cat.pcd";
-	writeClouds(stems,ss.str(),false);
-	for(int m=0;m<stems.size();m++)
+	writeClouds(stems, ss.str(), false);
+	for (int m = 0; m < stems.size(); m++)
 	{
 		ss.str("");
 		ss << id[0] << ".cluster." << m << ".pcd";
-		writer.write(ss.str(),*stems[m],true);
+		writer.write(ss.str(), *stems[m], true);
 	}
 	std::cout << ss.str() << " | " << stems.size() << std::endl;
 	//
